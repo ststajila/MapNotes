9f9c488f54ded260e3a865cbf5d3ac28046c354b
Stanislav <stazilastas@gmail.com>
2024-01-30 09:50:24 -0600

    Map View

--- MapNotes/ViewController.swift
+++ MapNotes/ViewController.swift
@@ -6,12 +6,40 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapViewOutlet: MKMapView!
+    let locationManager = CLLocationManager()
+    let locationManger = CLLocationManager()
+    var currntLocation: CLLocation!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManger.delegate =  self
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        locationManger.startUpdatingLocation()
+        
+        locationManager.requestWhenInUseAuthorization()
+        mapViewOutlet.showsUserLocation = true
+    
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center2 = locationManager.location!.coordinate
+        
+        let center = CLLocationCoordinate2D(latitude: 42.2371, longitude: -88.3226)
+        
+       let region = MKCoordinateRegion(center: center, latitudinalMeters: 1600, longitudinalMeters: 1600)
+        let region2 = MKCoordinateRegion(center: center, span: span)
+        
+        
+        mapViewOutlet.setRegion(region, animated: true)
+    
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currntLocation = locations[0]
     }
 
 
