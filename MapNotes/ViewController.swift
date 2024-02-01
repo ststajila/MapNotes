b6264e1fb5c3be27ae4cb36349f2cb2d459c1e36
Stanislav <stazilastas@gmail.com>
2024-02-01 09:51:07 -0600

    Parks on the map next to your location

--- MapNotes/ViewController.swift
+++ MapNotes/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var mapViewOutlet: MKMapView!
     let locationManager = CLLocationManager()
     let locationManger = CLLocationManager()
-    var currntLocation: CLLocation!
+    var currentLocation: CLLocation!
+    var parks: [MKMapItem] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +40,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        currntLocation = locations[0]
+        currentLocation = locations[0]
     }
     
     
@@ -47,8 +48,23 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let request = MKLocalSearch.Request()
         request.naturalLanguageQuery = "Parks"
         let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        request.region = MKCoordinateRegion(center: currntLocation.coordinate, span: span)
-        //8.18
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        
+        let search = MKLocalSearch(request: request)
+        
+        search.start { (response, error) in
+            guard let responce = response
+            else {
+                return
+            }
+            for mapItem in responce.mapItems{
+                self.parks.append(mapItem)
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = mapItem.placemark.coordinate
+                annotation.title = mapItem.name
+                self.mapViewOutlet.addAnnotation(annotation)
+            }
+        }
     }
     
 
